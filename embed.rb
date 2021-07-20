3e7d5ae11ec2903358120fa189b61c273020bbb6
Timothy Andrew <mail@timothyandrew.net>
2021-07-20 21:00:43 +0530

    add an embed version

---
+++ embed.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+if ARGV.count != 3
+  puts "usage: table.rb <year> <month> <days>"
+  exit 1
+end
+
+year, month, day = ARGV.map(&:to_i)
+
+(1..day).each do |day|
+  month_padded = sprintf '%02d', month
+  day_padded = sprintf '%02d', day
+  puts "![[#{year}-#{month_padded}-#{day_padded}]]"
+end
